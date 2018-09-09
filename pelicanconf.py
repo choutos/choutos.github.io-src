c78a6e19d42231dd8684c90f425783dd62bde651
choutos <maelocc@gmail.com>
2018-09-09 17:21:11 +0200

    Fix theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = 'choutos'
 SITENAME = 'choutos'
 THEME = '/home/choutos/src/blogue/pelican-themes/Flex'
-SITELOGO='images/profile.png'
+#SITELOGO='images/profile.png'
 FAVICON='images/favicon.ico'
 SITEURL = 'choutos.github.io'
 
