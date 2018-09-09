5f4fadab3e3497509c5270cd0ae9781cec0074f2
choutos <maelocc@gmail.com>
2018-09-09 17:29:55 +0200

    Add theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'choutos'
 SITENAME = 'choutos'
-THEME = 'Flex'
+THEME = 'pelican-themes/Flex'
 #SITELOGO='images/profile.png'
 #FAVICON='images/favicon.ico'
 SITEURL = 'choutos.github.io'
